35a3f69ca076acee391c39eacb6134ba673799ea
PhucHong-6113 <phuongdubu@gmail.com>
2025-03-19 23:15:10 +0700

    migrate notification

---
+++ HCP.Repository/Entities/PlatformNotification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCP.Repository.Entities
+{
+    public class PlatformNotification : BaseEntity
+    {
+        public string? Content { get; set; }
+        public bool IsRead { get; set; }
+        public string? ReturnUrl { get; set; }
+        public string? SenderId { get; set; }
+        public string? ReceiverId { get; set; }
+
+        // Navigation properties
+        public AppUser? Sender { get; set; }
+        public AppUser? Receiver { get; set; }
+    }
+}
---
+++ HCP.Repository/Migrations/20250319161339_AddNotification1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCP.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotification1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlatformNotifications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    ReturnUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SenderId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ReceiverId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlatformNotifications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlatformNotifications_AspNetUsers_ReceiverId",
+                        column: x => x.ReceiverId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PlatformNotifications_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlatformNotifications_ReceiverId",
+                table: "PlatformNotifications",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlatformNotifications_SenderId",
+                table: "PlatformNotifications",
+                column: "SenderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlatformNotifications");
+        }
+    }
+}
