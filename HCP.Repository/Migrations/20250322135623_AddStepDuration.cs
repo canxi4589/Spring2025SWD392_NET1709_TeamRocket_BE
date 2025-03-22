f453cd8bce71cf6623cf64bbe8063dbaf57721c4
PhucHong-6113 <phuongdubu@gmail.com>
2025-03-22 20:57:32 +0700

    Add Service Step Duration

---
+++ HCP.Repository/Migrations/20250322135623_AddStepDuration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HCP.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStepDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Duration",
+                table: "ServiceSteps",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "ServiceSteps");
+        }
+    }
+}
